58f7c183a59771826d093857b58894f791d0faff
Chikrix <itschidi5s@gmail.com>
2017-12-10 13:45:09 +0100

    Adds Views for location search

--- app/src/main/java/com/tutor/proteins/placetalk/modules/home/activities/WeatherActivity.kt
+++ app/src/main/java/com/tutor/proteins/placetalk/modules/home/activities/WeatherActivity.kt
@@ -2,16 +2,26 @@ package com.tutor.proteins.placetalk.modules.home.activities
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import com.tutor.proteins.placetalk.R
 import com.tutor.proteins.placetalk.R.layout
-import com.tutor.proteins.placetalk.databinding.ActivityHomeBinding
+import com.tutor.proteins.placetalk.databinding.WeatherActivityBinding
+import com.tutor.proteins.placetalk.modules.home.fragments.WeatherFragment
 
-class WeatherActivity : AppCompatActivity() {
-  private lateinit var homeActivityBinding: ActivityHomeBinding
+class WeatherActivity: AppCompatActivity() {
+  private lateinit var homeActivityBinding: WeatherActivityBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     homeActivityBinding = DataBindingUtil.setContentView(this,
-        layout.activity_home)
+        layout.weather_activity)
+    navigateToFragment()
+  }
+
+  private fun navigateToFragment() {
+    val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.weatherActivityContainer, WeatherFragment.newInstance())
+    fragmentTransaction.commit()
   }
 }
--- app/src/main/java/com/tutor/proteins/placetalk/modules/home/viewmodels/WeatherFragmentViewModel.kt
+++ app/src/main/java/com/tutor/proteins/placetalk/modules/home/viewmodels/WeatherFragmentViewModel.kt
@@ -1,4 +1,15 @@
 package com.tutor.proteins.placetalk.modules.home.viewmodels
 
-class WeatherFragmentViewModel {
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import com.tutor.proteins.placetalk.domain.repositories.OnlineRepository
+
+class WeatherFragmentViewModel(context: Application): AndroidViewModel(context) {
+
+  private val onlineRepository: OnlineRepository = OnlineRepository.INSTANCE
+
+  override fun onCleared() {
+
+  }
+
 }
